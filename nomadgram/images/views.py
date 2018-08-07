4d9f336317e2539cecb26455c396754a5a051cbf
dongjun77 <edj0707@naver.com>
2018-08-07 16:00:01 +0900

    image models serializer
    1. restframwork install
    2. make file serializers.py
    3. metadata model ,fiels
    4. views.py def get has return Response(data=serializer.data)

--- nomadgram/images/views.py
+++ nomadgram/images/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from . import models, serializers
 
-# Create your views here.
+class ListAllImages(APIVIEW):
+
+    def get(self, request, format=None):
+
+        all_images = models.Image.objects.all()
+
+        serializer = serializers.ImageSerializer(all_images, many=True)
+        # 그냥 시리얼 라이즈는 단수이다 mant=True를 통해 알려준다
+        return Response(data=serializer.data)
