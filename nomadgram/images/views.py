3863bf9a2aaf2a6f64e9b8b6257d98321272d4bf
dongjun77 <edj0707@naver.com>
2018-08-13 19:34:00 +0900

    Django users return
    is null -> users/views.py

--- nomadgram/images/views.py
+++ nomadgram/images/views.py
@@ -9,13 +9,26 @@ class Feed(APIView):
         user = request.user
 
         following_users = user.following.all()
-        print(following_users)
+        
+        image_list = []
 
         for following_user in following_users:
 
-            print(following_user.images.all()[:2])
+            user_images = following_user.images.all()[:2]
+
+            for image in user_images:
+                
+                image_list.append(image)
+
+        sorted_list = sorted(
+            image_list, key=lambda image: image.created_at, reverse=True)
+
+        serializer = serializers.ImageSerializer(sorted_list, many=True)
 
         return Response(status=200)
+
+# def get_key(image):
+#     return image.created_at
 
 # class ListAllImages(APIView):
 #     def get(self, request, format=None):
